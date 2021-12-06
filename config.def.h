def88ddeebd1c66a89cfcda048002ce335f1525e
khang <khanghugo123@gmail.com>
2021-12-05 20:43:24 -0500

    add colorbar and fix color

--- config.def.h
+++ config.def.h
@@ -11,19 +11,28 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Hack:size=11" };
+static const char dmenufont[]       = "Hack:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_orange[] 		= "#FF8000";
+static const char col_orange[] 		= "#FE8019";
 static const char col_mixed_green[] = "#8EC07C";
+static const char col_gruvbg[] 		= "#282828";
+static const char col_gruvfont[] 	= "#FBF1C7";
+static const char col_gruvfontdark[]= "#A89984";
+static const char col_gruvblue[]	= "#458588";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_mixed_green,  col_mixed_green  },
+	[SchemeStatus]  = { col_gruvfont, col_gruvbg,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  = { col_gruvfont, col_gruvblue,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+	[SchemeTagsNorm]  = { col_gruvfontdark, col_gruvbg,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+	[SchemeInfoSel]  = { col_gruvfont, col_gruvblue,  "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+	[SchemeInfoNorm]  = { col_gruvfontdark, col_gruvbg,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 /* tagging */
